7c28a1d96a5d0c5d34a3c094738237fa12c0ea96
Klaytonh10 <klaytonh10@gmail.com>
2025-09-30 14:05:08 -0500

    init push

--- src/main/java/com/pluralsight/CellPhoneService.java
+++ src/main/java/com/pluralsight/CellPhoneService.java
@@ -1,7 +1,22 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class CellPhoneService {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("What is the serial number: ");
+        int serialNumber = scanner.nextInt();
+        System.out.print("What is the model: ");
+        String model = scanner.nextLine();
+        System.out.print("What is the carrier: ");
+        String carrier = scanner.nextLine();
+        System.out.print("What is the phone number: ");
+        String phoneNumber = scanner.nextLine();
+        System.out.print("Who is the owner : ");
+        String owner = scanner.nextLine();
+
+        CellPhone phone1 = new CellPhone.PhoneValues(serialNumber, model, carrier, phoneNumber, owner);
     }
 }

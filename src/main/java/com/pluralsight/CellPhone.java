73fe0ca8e94659648da46cfd3cffd9a8f9706050
Klaytonh10 <klaytonh10@gmail.com>
2025-10-01 13:39:02 -0500

    restart commit

--- src/main/java/com/pluralsight/CellPhone.java
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -1,14 +1,12 @@
 package com.pluralsight;
 
 public class CellPhone {
+
     private int serialNumber;
     private String model;
     private String carrier;
     private String phoneNumber;
     private String owner;
 
-    public static String PhoneValues(int SN, String model, String carrier, String phoneNumber, String owner) {
-        String sentence = "";
-        return sentence;
-    }
+
 }

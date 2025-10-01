bcd1afe7fe5992d8d3a094fa3b3755354a7f504a
Klaytonh10 <klaytonh10@gmail.com>
2025-10-01 13:56:37 -0500

    Cellphone classes "done"

--- src/main/java/com/pluralsight/CellPhone.java
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -2,11 +2,45 @@ package com.pluralsight;
 
 public class CellPhone {
 
-    private int serialNumber;
-    private String model;
-    private String carrier;
-    private String phoneNumber;
-    private String owner;
+    private int serialNumber = 0;
+    private String model = "";
+    private String carrier = "";
+    private String phoneNumber = "";
+    private String owner = "";
+
+    public String getOwner() {
+        return this.owner;
+    }
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
 
 
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getCarrier() {
+        return this.carrier;
+    }
+    public void setCarrier(String carrier) {
+        this.carrier = carrier;
+    }
+
+    public int getSerialNumber() {
+        return this.serialNumber;
+    }
+    public void setSerialNumber(int serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
 }
--- src/main/java/com/pluralsight/CellPhoneService.java
+++ src/main/java/com/pluralsight/CellPhoneService.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 
 public class CellPhoneService {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        CellPhone cellPhone = new CellPhone();
 
     }
 }

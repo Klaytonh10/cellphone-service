1bbb96091e8a86a51c5f028bec966633c1ce8635
Klaytonh10 <klaytonh10@gmail.com>
2025-10-01 14:54:33 -0500

    prompt user

--- src/main/java/com/pluralsight/CellPhoneService.java
+++ src/main/java/com/pluralsight/CellPhoneService.java
@@ -4,7 +4,20 @@ import java.util.Scanner;
 
 public class CellPhoneService {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        CellPhone phone3 = new CellPhone();
+        CellPhone cellPhone1 = new CellPhone();
+        System.out.print("What is the serial number? ");
+        cellPhone1.setSerialNumber(scanner.nextInt());
+        System.out.print("What model is your phone? ");
+        cellPhone1.setModel(scanner.next());
+        System.out.println("Who is your carrier? ");
+        cellPhone1.setCarrier(scanner.next());
+        System.out.println("What is your phone number? ");
+        cellPhone1.setPhoneNumber(scanner.next());
+        System.out.println("Who is the owner of the phone? ");
+        cellPhone1.setOwner(scanner.next());
+
+        System.out.println(cellPhone1.getSerialNumber() + "\n" + cellPhone1.getModel() + "\n" + cellPhone1.getCarrier() + "\n" + cellPhone1.getPhoneNumber() + "\n" + cellPhone1.getOwner());
     }
 }
